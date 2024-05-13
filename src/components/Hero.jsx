a413c1de4dbf64c4e8b65010f32eaf35c1a5c404
lautaromateol <lautaromateol@gmail.com>
2024-05-13 12:05:39 -0300

    cambios en el hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
       <div className="mx-auto max-w-4xl">
         <img className="size-[5rem] mb-4 rounded-full" src="https://avatars.githubusercontent.com/u/103976582?v=4" alt="Foto de Lautaro Leguizamón" />
         <h1 className="text-4xl text-primary font-bold leading-tight mb-4">Hola! Soy Lautaro Leguizamón</h1>
-        <p className="text-2xl text-secondary mb-6 w-full lg:w-1/2">Desarrollador Full-Stack oriundo de Argentina. Apasionado por el desarrollo de aplicaciones web complejas.</p>
+        <p className="text-2xl text-secondary mb-6 w-full lg:w-1/2">Desarrollador Front-end oriundo de Argentina.</p>
         <div className="flex items-center gap-x-4">
           <a className="flex justify-center gap-1 px-4 py-1 rounded-full bg-gray-100 border border-gray-300 text-secondary text-base" target="_blank" href="https://github.com/lautaromateol">
             <IconBrandGithub stroke={1} />
