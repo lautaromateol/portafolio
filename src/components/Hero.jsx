387c5e57def82c1b512d8d47f957e92886ade460
lautaromateol <lautaromateol@gmail.com>
2024-05-13 12:15:31 -0300

    hero pantallas grandes arreglado

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
-import { IconBrandGithub, IconBrandLinkedin, IconMail } from "@tabler/icons-react"
+import { IconBrandGithub, IconBrandLinkedin } from "@tabler/icons-react"
 
 export default function Hero() {
   return (
     <section id="home" className="min-h-screen flex items-center justify-center px-6 lg:px-0">
-      <div className="mx-auto max-w-4xl">
+      <div className="max-w-4xl lg:w-1/2">
         <img className="size-[5rem] mb-4 rounded-full" src="https://avatars.githubusercontent.com/u/103976582?v=4" alt="Foto de Lautaro Leguizamón" />
         <h1 className="text-4xl text-primary font-bold leading-tight mb-4">Hola! Soy Lautaro Leguizamón</h1>
-        <p className="text-2xl text-secondary mb-6 w-full lg:w-1/2">Desarrollador Front-end oriundo de Argentina.</p>
+        <p className="text-2xl text-secondary mb-6 w-full">Desarrollador Front-end oriundo de Argentina.</p>
         <div className="flex items-center gap-x-4">
           <a className="flex justify-center gap-1 px-4 py-1 rounded-full bg-gray-100 border border-gray-300 text-secondary text-base" target="_blank" href="https://github.com/lautaromateol">
             <IconBrandGithub stroke={1} />
